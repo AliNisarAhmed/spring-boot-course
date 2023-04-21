b385eb82e6e9a9182a1ed758b68609b6b6c2693f
Ali Ahmed <ali.nisar87@gmail.com>
2023-04-20 23:13:22 -0600

    path variables aka route params

--- 04-restdemo/src/main/java/com/ali/restdemo/rest/StudentRestController.java
+++ 04-restdemo/src/main/java/com/ali/restdemo/rest/StudentRestController.java
@@ -4,22 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ali.restdemo.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
-  @GetMapping("/students")
-  public List<Student> getStudents() {
-    List<Student> theStudents = new ArrayList<>();
+  private List<Student> theStudents;
+
+  @PostConstruct
+  // called after the constructor when the dependencies are fully initialized
+  // ensures that this method will be invoked only ONCE in the bean lifecycle
+  public void LoadData() {
+    theStudents = new ArrayList<Student>();
     theStudents.add(new Student("Ali", "Ahmed"));
     theStudents.add(new Student("Samrah", "Akber"));
     theStudents.add(new Student("Azlan", "Ali"));
+
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
     return theStudents;
+  }
+
+  @GetMapping("/students/{studentId}")
+  public Student getStudent(@PathVariable int studentId)  {
+    return theStudents.get(studentId);
   }
 
 }
