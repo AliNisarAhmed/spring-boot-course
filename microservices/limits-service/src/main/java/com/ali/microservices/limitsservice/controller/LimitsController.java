aa10d2c854538cb86bb187bc1099a3c08114c6c3
Ali Ahmed <ali.nisar87@gmail.com>
2023-06-24 22:27:10 -0600

    microservices: set-up limits service, config server and localconfig

---
+++ microservices/limits-service/src/main/java/com/ali/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.ali.microservices.limitsservice.controller;
+
+import com.ali.microservices.limitsservice.bean.Limits;
+import com.ali.microservices.limitsservice.configuration.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    private final Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
